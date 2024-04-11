0a72ca08188eabbb31d2c7052b9718cc48509d67
Ulises Saucedo <147457362+Ulises-Saucedo@users.noreply.github.com>
2024-04-11 13:33:38 -0300

    Initial commit

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,15 +1,6 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-    },
-    surname: {
-        type: String,
-    },
-    age: {
-        type: Number,
-    },
     username: {
         type: String,
         required: true,
